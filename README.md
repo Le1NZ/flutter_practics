cfffca95f36d9ce7a983f1bab29d71e3feeabcfb
POKROVSKII <ppavel06102004@gmail.com>
2025-09-27 00:12:17 +0300

    Practice 2: Add base architecture and texts to main screen

--- README.md
+++ README.md
@@ -1,16 +1,17 @@
-# flutter_project
+# Simple Flutter Project
 
-flutter_project
+Простой Flutter-проект для учебных целей: обучения Flutter и Dart.
 
-## Getting Started
+## Архитектура
 
-This project is a starting point for a Flutter application.
-
-A few resources to get you started if this is your first Flutter project:
-
-- [Lab: Write your first Flutter app](https://docs.flutter.dev/get-started/codelab)
-- [Cookbook: Useful Flutter samples](https://docs.flutter.dev/cookbook)
-
-For help getting started with Flutter development, view the
-[online documentation](https://docs.flutter.dev/), which offers tutorials,
-samples, guidance on mobile development, and a full API reference.
+```text
+lib/
+├── src/
+│ ├── core/
+│ │ └── design/  # Дизайн-система
+│ │
+│ └── feature/
+│   └── home/    # Фича "Главный экран"
+│
+└── main.dart    # Точка входа в приложение
+```
---
+++ lib/src/feature/home/src/home_screen.dart
@@ -0,0 +1,76 @@
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          spacing: 8,
+          children: [
+            const _Name(),
+            const _GroupNumber(),
+            const _StudentIdCardNumber(),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class _Name extends StatelessWidget {
+  const _Name();
+
+  final String name = 'Покровский Павел Дмитривевич';
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      name,
+      style: TextStyle(
+        fontSize: 24,
+        color: Colors.black,
+        fontWeight: FontWeight.bold,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+class _GroupNumber extends StatelessWidget {
+  const _GroupNumber();
+
+  final String groupNumber = 'ИКБО-06-22';
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      groupNumber,
+      style: TextStyle(fontSize: 20, color: Colors.black),
+      textAlign: TextAlign.center,
+    );
+  }
+}
+
+class _StudentIdCardNumber extends StatelessWidget {
+  const _StudentIdCardNumber();
+
+  final String cardNumber = '22И1204';
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      cardNumber,
+      style: TextStyle(
+        fontSize: 20,
+        color: Colors.black,
+        fontStyle: FontStyle.italic,
+      ),
+      textAlign: TextAlign.center,
+    );
+  }
+}
