ed1483e3bef875f97f25b9990922c911ef30ae1b
POKROVSKII <ppavel06102004@gmail.com>
2025-11-15 00:40:18 +0300

    Practice-9: Remove Di and add Cubit to deps

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_project/app.dart';
-import 'package:flutter_project/shared/service_locator.dart';
 
 void main() {
-  setupLocator();
   runApp(PetApp());
 }
