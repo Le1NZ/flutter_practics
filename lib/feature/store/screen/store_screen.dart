1fb3fe217ff29039bfb7a78a58d369dddf0ddf18
POKROVSKII <ppavel06102004@gmail.com>
2025-11-15 00:41:00 +0300

    Practice-9: Add Cubit to store screen

--- lib/feature/store/screen/store_screen.dart
+++ lib/feature/store/screen/store_screen.dart
@@ -1,22 +1,15 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_project/shared/service/user_service.dart';
-import 'package:flutter_project/shared/service_locator.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_project/feature/store/cubit/store_cubit.dart';
+import 'package:flutter_project/feature/pet_info/cubit/user_info_cubit.dart';
 
+import '../../pet_info/model/user_info.dart';
 import '../model/store_item.dart';
 import '../model/store_item_state.dart';
 import '../widget/store_item_view.dart';
 
-class StoreScreen extends StatefulWidget {
-  const StoreScreen({
-    super.key,
-  });
-
-  @override
-  State<StoreScreen> createState() => _StoreScreenState();
-}
-
-class _StoreScreenState extends State<StoreScreen> {
-  final _userService = locator<UserService>();
+class StoreScreen extends StatelessWidget {
+  const StoreScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -24,43 +17,53 @@ class _StoreScreenState extends State<StoreScreen> {
       appBar: AppBar(title: const Text("Магазин")),
       body: Padding(
         padding: const EdgeInsets.all(16),
-        child: _info(context),
+        child: BlocBuilder<StoreCubit, StoreState>(
+          builder: (context, storeState) {
+            return BlocBuilder<UserInfoCubit, UserInfo>(
+              builder: (context, userInfoState) {
+                return _info(context, storeState, userInfoState);
+              },
+            );
+          },
+        ),
       ),
     );
   }
 
-  Widget _info(BuildContext context) {
-    final availableMoney = _userService.userInfo.money;
-
+  Widget _info(
+    BuildContext context,
+    StoreState storeState,
+    UserInfo userInfoState,
+  ) {
     return Column(
       children: [
         Text(
-          "Монеты: $availableMoney",
+          "Монеты: ${userInfoState.money}",
           style: const TextStyle(fontSize: 16.0, color: Colors.black),
         ),
-        Expanded(child: _itemsList(context)),
+        Expanded(child: _itemsList(context, storeState, userInfoState)),
       ],
     );
   }
 
-  Widget _itemsList(BuildContext context) {
+  Widget _itemsList(
+    BuildContext context,
+    StoreState storeState,
+    UserInfo userInfoState,
+  ) {
     return ListView.builder(
       itemBuilder: (context, index) {
-        final item = _userService.storeItems[index];
+        final item = storeState.items[index];
         return StoreItemView(
           key: ValueKey(item),
           name: item.name,
           price: item.price,
           imageUrl: item.imageUrl,
-          state: _resolveItemState(item, _userService.userInfo.money),
-          onBuyPressed: () {
-            setState(() {
-              _userService.buyItem(item.id);
-            });
-          },
+          state: _resolveItemState(item, userInfoState.money),
+          onBuyPressed: () => context.read<StoreCubit>().buyItem(item),
         );
       },
-      itemCount: _userService.storeItems.length,
+      itemCount: storeState.items.length,
     );
   }
 
